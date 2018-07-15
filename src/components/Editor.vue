0f75cab4e13c0578d0b582fb78087e66f5718999
takayuki.kayawari <kayamelo@gmail.com>
2018-07-15 16:44:02 +0900

    googleログイン処理追加

---
+++ src/components/Editor.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='editor'>
+    <h1>Editor Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'editor',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+</style>
