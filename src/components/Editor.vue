3e181166692dfe77e38204e6cfe3543722d64b9e
takayuki.kayawari <kayamelo@gmail.com>
2018-07-15 23:08:01 +0900

    メモを複数管理ができるようにする

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -5,8 +5,14 @@
     <div>
       <button @click='logout'>ログアウト</button>
     </div>
+    <div class="memoListWrapper">
+      <div class="memoList" v-for="(memo, index) in memos" @click="selectMemo(index)" :data-selected="index == selectedIndex">
+        <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
+      </div>
+      <button class="addMemoBtn" @click="addMemo">add Memo</button>
+    </div>
     <div class='editorWrapper'>
-      <textarea class='markdown' v-model='markdown'></textarea>
+      <textarea class='markdown' v-model='memos[selectedIndex].markdown'></textarea>
       <div class='preview' v-html='preview()'></div>
     </div>
   </div>
@@ -20,7 +26,10 @@ export default {
   props: ['user'],
   data () {
     return {
-      markdown: '',
+      memos:[{
+        markdown: ''
+      }],
+      selectedIndex: 0,
     }
   },
   methods: {
@@ -28,13 +37,53 @@ export default {
       firebase.auth().signOut();
     },
     preview: function() {
-      return marked(this.markdown);
-    }
+      return marked(this.memos[this.selectedIndex].markdown);
+    },
+    addMemo:function(){
+      this.memos.push({
+        markdown: '無題のメモ',
+      })
+    },
+    selectMemo: function(index) {
+      this.selectedIndex = index;
+    },
+    displayTitle: function(text) {
+      return text.split(/\n/)[0];
+    },
   }
 }
 </script>
 
 <style lang='scss'>
+.memoListWrapper {
+	width: 19%;
+	float: left;
+	border-top: 1px solid #000;
+}
+.memoList {
+	padding: 10px;
+	box-sizing: border-box;
+	text-align: left;
+	border-bottom: 1px solid #000;
+	
+	&:nth-child(even) {
+		background-color: #ccc;
+	}
+	
+	&[data-selected="true"] {
+		background-color: #ccf;
+	}
+}
+.memoTitle {
+	height: 1.5em;
+	margin: 0;
+	white-space: nowrap;
+	overflow: hidden;
+}
+.addMemoBtn {
+	margin-top: 20px;
+}
+
 .editorWrapper{
 	display: flex;
 	}
